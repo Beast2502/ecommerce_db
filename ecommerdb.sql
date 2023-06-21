ded9537273e6a7e36df8221983dd8cf5db62ddfa
Mehul Saxena <MSaxena@Arenaco.com>
2023-06-21 17:32:36 +0530

    tables structure updates

--- ecommerdb.sql
+++ ecommerdb.sql
@@ -6,28 +6,32 @@ create table adminUsers (
 	role_name varchar(100) not null,
 	pass_word varchar(200) not null,
 	isActive boolean not null,
-	createDate date not null
+	created_at date not null,
+	modified_at date not null
 )
 
 create table roles(
 	role_id bigserial not null primary key,
 	role_name varchar(100) not null,
 	isActive boolean not null,
-	createDate date not null
+	created_at date not null,
+	modified_at date not null
 )
 
 create table products (
 	product_id bigserial not null primary key,
 	product_name varchar(100) not null,
+	product_desc varchar(500) not null,
 	image_url text not null,
 	price int not null,
 	mrp int not null,
 	category varchar(200) not null,
-	quantity int not null,
+	inventory int not null,
 	sku varchar(200) not null,
 	on_sale boolean not null,
 	isActive boolean not null,
-	createDate date not null
+	created_at date not null,
+	modified_at date not null
 )
 
 create table categories(
@@ -43,21 +47,42 @@ create table users(
 	email varchar(500) not null,
 	pass_word varchar(500) not null,
 	mobile_no varchar(100) not null,
-	address varchar(500) not null,
-	pincode varchar(500) not null	
+	created_at date not null,
+	modified_at date not null
 )
 
-create table orders(
+create table users_address(
+	id bigserial not null,
+	user_id bigserial not null,
+	address_line1 varchar (500) not null,
+	address_line2 varchar(500) not null,
+	city varchar(500) not null,
+	postal_code varchar(500) not null,
+	country varchar(500) not null,
+	mobile varchar(500) not null,
+)
+
+create table ordersdetails(
 	order_id varchar(500) not null,
 	user_id varchar(500) not null,
-	product_id varchar(500) not null,
-	category_id varchar(500) not null,
+	
+	
 	quantity varchar(500) not null,
 	amount_paid varchar(500) not null,
 	address varchar(500) not null,
 	delivery_status varchar(500) not null,
-	createDate date not null,
+	created_at date not null,
+	modified_at date not null
 ) 
+
+create table order_items(
+	order_id varchar(500) not null,
+	product_id varchar(500) not null,
+	category_id varchar(500) not null,
+	quantity varchar(500) not null,
+	created_at date not null,
+	modified_at date not null
+)
 
 
 
